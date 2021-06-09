361641a0002df2df936aeddcb6cb3b61ef340972
Kris Hopper <hopperdavis@gmail.com>
2021-06-09 14:22:27 -0700

    pass 2nd test

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,20 @@
 // Business Logic
 function pigLatin(word) {
 
-  if(word[0] === 'a' || word[0] === 'A' || word[0] === 'e' || word[0] === 'E' || word[0] === 'i' || word[0] === 'I' || word[0] === 'o' || word[0] === 'O' || word[0] === 'u' || word[0] === 'U')
-  {
-      return word + "way";
-  }
+  if(word[0] === 'a' || word[0] === 'A' || word[0] === 'e' || word[0] === 'E' || word[0] === 'i' || word[0] === 'I' || word[0] === 'o' || word[0] === 'O' || word[0] === 'u' || word[0] === 'U'){
 
-  //Test: "For words that begin with a consonant, it will move the consonant to the end of the word and add 'ay'."
-  //Code: pigLatin("book");
-  //Expected Output: "ookbay"
+    return word + "way";
+  }
+  else{
+    return word.slice(1)  + word.slice(0,1) + "ay";
+
+  }
+  
+
+
+  // Test: "For words that begin with more than one consonant, it will move all consonants before the first vowel to the end of the word and add 'ay'."
+  // Code: pigLatin("stop");
+  // Expected Output: "opstay"
 
 }
 
