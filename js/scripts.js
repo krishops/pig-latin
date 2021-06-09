f682591426232e2ab3ee341ec12be0ff29b2ba6e
Kris Hopper <hopperdavis@gmail.com>
2021-06-09 15:12:54 -0700

    all tests pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,33 @@
 // Utility Logic
-function checkVowel (text) {
-  if(text === 'a' || text === 'A' || text === 'e' || text === 'E' || text === 'i' || text === 'I' || text === 'o' || text === 'O' || text === 'u' || text === 'U')
-  {
+function checkVowel(text) {
+  if (text === 'a' || text === 'A' || text === 'e' || text === 'E' || text === 'i' || text === 'I' || text === 'o' || text === 'O' || text === 'u' || text === 'U') {
     return true;
   }
-  else 
-  {
+  else {
     return false;
   }
 }
 // Business Logic
 function pigLatin(word) {
-
-  if( checkVowel(word[0]) ){
+  if (checkVowel(word[0])) {
     return word + "way";
   }
-  else{  
-    if ( (word[0] === 'q' || word[0] === 'Q' ) && (word[1] === 'u' || word[1] === 'U') )
-    {
-      return word.slice(2) + word.slice(0,2) + "ay";
+  else {
+    if ((word[0] === 'q' || word[0] === 'Q') && (word[1] === 'u' || word[1] === 'U')) {
+      return word.slice(2) + word.slice(0, 2) + "ay";
     }
 
-    let marker=0;
-    for(let index=0; index<word.length; index++)
-    {
-      if (checkVowel(word[index]))
-      {
-        marker = index;
-        break;
+    let marker = 0;
+    for (let index = 0; index < word.length; index++) {
+      if (checkVowel(word[index])) {
+        if (!(word[index] === 'u' && word[index - 1] === 'q')) {
+          marker = index;
+          break;
+        }
       }
     }
-    return word.slice(marker)  + word.slice(0,marker) + "ay";
+    return word.slice(marker) + word.slice(0, marker) + "ay";
   }
-  
-  // Test: "For words that begin with 'q' not followed by a u, it will follow the same rule as other consonants."
-  // Code: pigLatin("qi");
-  // Expected Output: "iqay"
-
-
-
 }
 
 
